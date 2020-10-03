a240b089147cf792e20a64c4a696495c49c5aaec
Akshay <akshaybhopani@gmail.com>
2020-10-03 16:44:00 +0530

    Automatic Contact-Us Form

---
+++ mail.php
@@ -0,0 +1,44 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$subject = $_POST['subject'];
+$content="From: $name \n Email: $email \n Message: $message";
+$recipient = "YOURNAME@DOMAIN.COM";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $content, $mailheader) or die("Mail Server Error!");
+echo '
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="../style-success.css" rel="stylesheet">
+	<title>Success</title>
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400|Montserrat:700" rel="stylesheet" type="text/css">
+	<style>
+		@import url(//cdnjs.cloudflare.com/ajax/libs/normalize/3.0.1/normalize.min.css);
+		@import url(//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css);
+	</style>
+	<script src="https://2-22-4-dot-lead-pages.appspot.com/static/lp918/min/jquery-1.9.1.min.js"></script>
+	<script src="https://2-22-4-dot-lead-pages.appspot.com/static/lp918/min/html5shiv.js"></script>
+</head>
+<body>
+	<div class="background-success">
+     <div class="card">   
+     <div class="main wow fadeInUp" style="visibility: visible; animation-name: fadeInUp;">                
+	<header class="site-header" id="header">
+		<h1 class="site-header__title" data-lead-id="site-header-title">THANK YOU!</h1>
+	</header>
+
+	<div class="main-content">
+		<i class="fa fa-check main-content__checkmark fa-3x" id="checkmark"></i>
+		<p class="main-content__body" data-lead-id="main-content-body">Thanks a bunch for filling that out. It means a lot to us, just like you do! We really appreciate you giving us a moment of your time today. Thanks for being you.</p>
+	</div>
+	</div></div></div>
+</div>
+</body>
+</html>
+
+';
+?>
